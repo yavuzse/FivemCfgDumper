4ede943c6b4a723a60440838be88515d74ed7622
Yavuz Selim <57921834+yavuzse@users.noreply.github.com>
2021-07-20 19:05:26 +0200

    Create dmp.lua

---
+++ dmp.lua
@@ -0,0 +1,24 @@
+function GetResources()
+    local resources = {}
+    for i = 1, GetNumResources() do
+        resources[i] = GetResourceByFindIndex(i)
+    end
+    return resources
+end
+
+
+function DumpCfg()
+	local Resources = GetResources()
+	
+	for i = 1, #Resources do
+		Wait(Time)
+		current = Resources[i]
+		for k, v in pairs({'config.lua'}) do
+			data = LoadResourceFile(current, v)
+			if data ~= nil then
+                print(data)
+			end
+		end
+	end
+		
+end
